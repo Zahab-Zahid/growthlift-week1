a06572359c48e85a9ac1431382622ee33948428e
Zahab Zahid <zahabzahid2005@gmail.com>
2026-07-08 23:15:53 +0500

    Day 10: JavaScript fundamentals

---
+++ script.js
@@ -0,0 +1,91 @@
+let name = "Zahab Zahid";
+console.log(name);
+
+name = "Zahab";
+console.log(name);
+
+const site = "GrowthLift";
+console.log(site);
+
+var oldWay = "Avoid using var";
+console.log(oldWay);
+
+
+
+
+
+
+
+
+let greeting = "Hello World";
+console.log(greeting);
+
+let price = 15000;
+console.log(price);
+
+let isOnline = true;
+console.log(isOnline);
+
+let skills = ["HTML", "CSS", "JavaScript"];
+console.log(skills);
+
+let intern = {
+    name: "Zahab",
+    week: 2,
+    city: "Lahore"
+};
+
+console.log(intern);
+
+function greet(name){
+    return "Hello " + name;
+}
+
+console.log(greet("Zahab"));
+
+const greetArrow = (name) => "Hello " + name;
+
+console.log(greetArrow("GrowthLift"));
+
+
+
+
+
+
+
+
+
+for(let i = 0; i < 5; i++){
+    console.log(i);
+}
+
+let skillsList = ["HTML","CSS","JavaScript"];
+
+skillsList.forEach(function(skill){
+    console.log(skill);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+if(isOnline){
+    console.log("User is online");
+}
+else{
+    console.log("Offline");
+}
+
+
+
+
+
+
+console.log(`Welcome to ${site}, ${name}!`);
